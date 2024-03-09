0d16dbf934a89d0b328603b3416b81ac3e43c194
andy <66240551+flandolf@users.noreply.github.com>
2024-03-09 13:15:41 +1100

    fixes + office 95 support

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ This is a Windows 95 Keygen based on public methods (and a really simple algorit
 
 ## How does it work?
 
-Sample key: AAABB-OEM-0CCCCCC-DDDDD
-The first 3 digits are any number from 1-366, signifying a day of the year.
-The next 2 are a year, anything from 95-03
-The C section is any number when added up, is divisible by zero, its last digit cannot be 0 or >= 8.
-The D section is random numbers.
+Sample key: AAABB-OEM-0CCCCCC-DDDDD  
+The first 3 digits are any number from 1-366, signifying a day of the year.  
+The next 2 are a year, anything from 95-03  
+The C section is any number when added up, is divisible by zero, its last digit cannot be 0 or >= 8.  
+The D section is random numbers.  
 
 ## Build
 
@@ -18,4 +18,4 @@ cargo build
 
 ## Disclaimer
 
-This software, referred to as 95Gen, is a project based on public methods and a simple algorithm designed for educational and recreational purposes only. The primary goal is to explore and understand basic key generation concepts. Windows 95 is essentially abandonware, as there are no future updates planned for the OS. 
+This software, referred to as 95Gen, is a project based on public methods and a simple algorithm designed for educational and recreational purposes only. The primary goal is to explore and understand basic key generation concepts. Windows 95 is essentially abandonware, as there are no future updates planned for the OS.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use catppuccin_egui::MACCHIATO;
+#![windows_subsystem = "windows"]
+
 use eframe::egui::{self, Label, ScrollArea};
 use rand::Rng;
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([640.0, 480.0]),
+        follow_system_theme: true,
         ..Default::default()
     };
     eframe::run_native(
@@ -14,9 +16,16 @@ fn main() -> Result<(), eframe::Error> {
     )
 }
 
+#[derive(Clone, Copy, PartialEq)]
+enum KeyType {
+    Windows95,
+    Office95,
+}
+
 struct MainApp {
     amount: i32,
     keys: Vec<String>,
+    key_type: KeyType,
 }
 
 impl Default for MainApp {
@@ -24,25 +33,33 @@ impl Default for MainApp {
         Self {
             amount: 1,
             keys: Vec::new(),
+            key_type: KeyType::Windows95, // Default to Windows 95 keys
         }
     }
 }
 
 impl eframe::App for MainApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        catppuccin_egui::set_theme(ctx, MACCHIATO);
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Windows 95 Keygen - by @flandolf");
+            ui.heading("Key Generator - by @flandolf");
 
             ui.horizontal(|ui| {
                 ui.label("Amount of keys to generate:");
-                ui.add(egui::Slider::new(&mut self.amount, 1..=1000));
+                ui.add(egui::Slider::new(&mut self.amount, 1..=500));
+            });
+
+            ui.horizontal(|ui| {
+                ui.radio_value(&mut self.key_type, KeyType::Windows95, "Windows 95");
+                ui.radio_value(&mut self.key_type, KeyType::Office95, "Office 95");
             });
 
             if ui.button("Generate").clicked() {
                 self.keys.clear();
                 for _ in 0..self.amount {
-                    let key = generate_key();
+                    let key = match self.key_type {
+                        KeyType::Windows95 => generate_key_95(),
+                        KeyType::Office95 => generate_key_office95(),
+                    };
                     self.keys.push(key);
                 }
             }
@@ -58,19 +75,43 @@ impl eframe::App for MainApp {
     }
 }
 
-fn generate_key() -> String {
+fn generate_key_95() -> String {
     let day = rand::thread_rng().gen_range(1..366);
     let year = rand::thread_rng().gen_range(95..99);
     let algodigits = gen_algo_key();
     let randomend = rand::thread_rng().gen_range(10000..99999);
-
-    if day < 100 {
-        format!("0{}{}-OEM-0{}-{}", day, year, algodigits, randomend)
-    } else if day < 10 {
+    if day < 10 {
         format!("00{}{}-OEM-0{}-{}", day, year, algodigits, randomend)
+    } else if day < 100 {
+        format!("0{}{}-OEM-0{}-{}", day, year, algodigits, randomend)
     } else {
         format!("{}{}-OEM-0{}-{}", day, year, algodigits, randomend)
     }
+}
+
+fn generate_key_office95() -> String {
+    let mut rng = rand::thread_rng();
+
+    // Generate a random 3-digit number excluding specific values
+    let first_three_digits: u16 = loop {
+        let num = rng.gen_range(100..1000);
+        if num != 333 && num != 444 && num != 555 && num != 666 && num != 777 && num != 888 && num != 999 {
+            break num;
+        }
+    };
+
+    // Generate a random 7-digit number divisible by 21
+    let mut next_seven_digits: u32 = loop {
+        let num = rng.gen_range(1000000..10000000);
+        if num % 21 == 0 {
+            break num;
+        }
+    };
+
+    // Format the key
+    let key = format!("{:03}-{}", first_three_digits, next_seven_digits);
+
+    key
 }
 
 fn gen_algo_key() -> i32 {
